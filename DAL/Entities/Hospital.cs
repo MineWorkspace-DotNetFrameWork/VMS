3938f7e3e0ee56164d305a810d7c115c82712bad
Student <student@lan.itstep.kiev.ua>
2021-05-10 18:59:27 +0300

    Finished with Entities

---
+++ DAL/Entities/Hospital.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+
+        public virtual IEnumerable<Doctor> Doctors { get; set; }
+        public virtual IEnumerable<Point> Points { get; set; }
+    }
+}
---
+++ DAL/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Birth { get; set; }
+        public DateTime? First { get; set; }
+        public DateTime? Second { get; set; }
+        
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual IEnumerable<Journal> Journals { get; set; }
+    }
+}
