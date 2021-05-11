9f4a8c263004712e2d3950b1467fffee3b872434
Student <student@lan.itstep.kiev.ua>
2021-05-11 18:49:41 +0300

    Added DataManager

---
+++ DAL/ORM/DataManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using DAL.Entities;
+
+namespace DAL.ORM
+{
+    public class DataManager : DbContext
+    {
+        public DataManager()
+            : base("name=DataManager")
+        {
+        }
+        public virtual DbSet<Category> MyEntities { get; set; }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+        public virtual DbSet<Journal> Journals { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Point> Points { get; set; }
+        public virtual DbSet<Producer> Producers { get; set; }
+        public virtual DbSet<Vaccine> Vaccines { get; set; }
+
+    }
+}
