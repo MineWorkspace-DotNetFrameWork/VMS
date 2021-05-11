732a6a12d2a8f8c98532507c9a78e1a4517831bd
Student <student@lan.itstep.kiev.ua>
2021-05-11 18:17:28 +0300

    Finished with Models

--- DAL/Entities/Producer.cs
+++ DAL/Entities/Producer.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DAL.Entities
 {
     public class Producer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
         public int CountryId { get; set; }
         public virtual Country Country { get; set; }
